ceaac5971a6d6a0fa0f66b9248ed3bf22cfc97da
LAPTOP-QETVEFIT\Admin <trankhanhlc99@gmail.com>
2021-01-08 08:48:14 +0700

    Khánh sáng ngày 1/8/2020

---
+++ Assets/Game/Scrip/Core/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 post;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Rotate();
+    }
+    public void _Rotate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            post = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            transform.DORotate(new Vector3(0, 0, post.x) * 25, 0.01f);
+
+
+        }
+
+    }
+}
