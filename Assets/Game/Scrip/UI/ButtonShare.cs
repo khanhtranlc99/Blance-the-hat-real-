38221959efc37a5c8c6b17832382aae1bb429eec
LAPTOP-QETVEFIT\Admin <trankhanhlc99@gmail.com>
2021-02-17 22:45:26 +0700

    Share Button

---
+++ Assets/Game/Scrip/UI/ButtonShare.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ButtonShare : MonoBehaviour
+{
+	private string messenger;
+	private Coroutine messengerCoroutin; 
+	public void _OnClickBtnShare()
+    {
+		messenger = "I scored " + GameCoreManager.coreManager.coutnTime + "in Impostor Balance by @OneSoft Global PTE. LTD.";
+		if ( messengerCoroutin != null)
+        {
+			messengerCoroutin = null;
+        }
+		messengerCoroutin = StartCoroutine(TakeScreenshotAndShare());
+
+	}
+	private IEnumerator TakeScreenshotAndShare()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
+
+		string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+		File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+		// To avoid memory leaks
+		Destroy(ss);
+
+		new NativeShare().AddFile(filePath)
+			.SetSubject("Subject goes here").SetText(messenger)
+			.SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+			.Share();
+
+		// Share on WhatsApp only, if installed (Android only)
+		//if( NativeShare.TargetExists( "com.whatsapp" ) )
+		//	new NativeShare().AddFile( filePath ).AddTarget( "com.whatsapp" ).Share();
+	}
+}
